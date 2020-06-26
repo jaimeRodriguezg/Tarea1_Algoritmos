e718b2c76d3625d8164515d9ac06d7b1905c308f
Valeria Paz Miranda Martínez <valeria.mirandam@sansano.usm.cl>
2020-06-25 23:32:30 -0400

    commit stdin y stdout

--- Pregunta1/main.py
+++ Pregunta1/main.py
@@ -1,5 +1,4 @@
-#nombre = input("Ingrese su nombre: ")
-#print("Hola "+nombre+", welcome back to python")
+'''
 def comprobar(atributos,tupla1,tupla2):
     resultado=0
     for i in atributos:
@@ -56,3 +55,48 @@ for r in respuesta:
     fw.write(r)
 fr.close()
 fw.close()
+'''
+def comprobar(atributos,tupla1,tupla2):
+    resultado=0
+    for i in atributos:
+        pos_R=atributos_R.index(i)
+        pos_S=atributos_S.index(i)
+        if tupla1[pos_R]==tupla2[pos_S]:
+            resultado+=1
+    if resultado==len(atributos):
+        return True
+    else: return False
+cant_at_R = int(input(":").strip())
+atributos_R = input(":").strip().split()
+cant_tuplas_R = int(input(":").strip())
+lista_tuplas_R=[]
+for i in range(cant_tuplas_R):
+    lista_tuplas_R.append(tuple(input(":").strip().split()))
+cant_at_S = int(input(":").strip())
+atributos_S = input(":").strip().split()
+cant_tuplas_S = int(input(":").strip())
+lista_tuplas_S=[]
+for i in range(cant_tuplas_S):
+    lista_tuplas_S.append(tuple(input(":").strip().split()))
+print("********************")
+atributos_de_join=list(set(atributos_R)&set(atributos_S))
+atributos_en_orden=list(set(atributos_R)|set(atributos_S))
+atributos_en_orden.sort()
+print(str(len(atributos_en_orden)))
+for i in atributos_en_orden:
+    print(i+" ",end="")
+print("")
+respuesta=[]
+for tupla_R in lista_tuplas_R:
+    for tupla_S in lista_tuplas_S:
+        if comprobar(atributos_de_join,tupla_R,tupla_S):
+            linea=""
+            for x in atributos_en_orden:
+                if x in atributos_R:
+                    linea=linea+tupla_R[atributos_R.index(x)]+" "
+                else:
+                    linea=linea+tupla_S[atributos_S.index(x)]+" "
+            respuesta.append(linea)
+print(str(len(respuesta)))
+for r in respuesta:
+    print(r)
